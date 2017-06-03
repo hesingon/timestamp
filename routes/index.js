26537e6f7e1282e022f8535ef213627ab8f55fe8
He Xing Darius <E0007888@u.nus.edu>
2017-06-03 18:20:41 +0800

    set up heroku

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,33 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express', name: 'Darius'});
 });
 
+router.get('/:time', function(req, res){
+
+    var unixToNatural = function(time){
+        var date = new Date(time * 1000)
+        var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December']
+        var day = date.getDate(date)
+        var month = months[date.getMonth(date)]
+        var year = date.getFullYear(date)
+
+        return month + ' ' + day + ', ' + year
+    }
+
+    if(!isNaN(req.params.time)){
+        var result = { 'unix' : req.params.time, 'natural': unixToNatural(req.params.time)}
+        res.json(result)
+    } else {
+        var natural = new Date(req.params.time)
+        var result
+        if(!isNaN(natural)){
+            var unix = natural / 1000
+            result = { 'unix': unix, 'natural': req.params.time}
+        } else {
+            result = { 'unix': null, 'natural': null}
+        }
+        res.json(result)
+    }
+    
+})
+
 module.exports = router;
